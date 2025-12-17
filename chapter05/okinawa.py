ebafd595b789676a9513feea0c6837e32eceb19c
itc-n24016 <n24016@std.it-college.ac.jp>
2025-12-17 14:10:32 +0900

    12/17 辞書 沖縄

---
+++ chapter05/okinawa.py
@@ -0,0 +1,12 @@
+from orca.settings import orcaModifierKeys
+
+okinawa = {
+    "県花": "デイゴ",
+    "県鳥": "ノグチゲラ",
+    "県魚": "タカサゴ",
+    "県のお菓子": "サーターアンダギー"
+}
+print(okinawa.keys())
+keys = input(f"選択したのは？ -> ")
+
+print(okinawa[keys])

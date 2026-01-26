7d66b56631a7e6db78f0e5d5528151cd0de7d859
itc-n24016 <n24016@std.it-college.ac.jp>
2026-01-26 16:44:07 +0900

    1/26 課題　final

--- kadai/four_operations.py
+++ kadai/four_operations.py
@@ -3,5 +3,9 @@ b = int(input(f"2つ目の整数を入力"))
 print(f"和：{a + b}")
 print(f"差：{a - b}")
 print(f"積：{a * b}")
-print(f"商：{a // b}")
-print(f"剰余：{a % b}")
+if b != 0:
+    print(f"商：{a // b}")
+    print(f"剰余：{a % b}")
+    print(f"exit")
+else:
+   print(f"exit")
